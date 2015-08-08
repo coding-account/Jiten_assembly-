2a44ba79721efa3f4a53253523ebea5f63446294
coding-account <gorkhali_jiten@yahoo.com>
2015-08-08 17:37:50 -0500

    Update p2_1000864824.s

--- p2_1000864824.s
+++ p2_1000864824.s
@@ -2,72 +2,72 @@
 .func main
 
 main:
-        BL _getOperand
-        MOV R6, R0
-        BL _getOperand
-        MOV R7, R0
-        MOV R1, R6
-        MOV R2, R7
-        BL GCD_ITERATIVE
-        MOV R1, R0
-        BL _displayPromptComputation
-        B main
+        BL _getOperand          @ branch to _getOperand
+        MOV R6, R0              @ move return value from R0 to R6
+        BL _getOperand          @ branch to _getOperand
+        MOV R7, R0              @ move return value from R0 to R7
+        MOV R1, R6              @ move from R6 to R1
+        MOV R2, R7              @ move from R7 to R2
+        BL GCD_ITERATIVE        @ branch to GCD_ITERATIVE
+        MOV R1, R0              @ move return value from R0 to R1
+        BL _displayPromptComputation    @ branch to _displayPromptComputation 
+        B main                  @ calling main  so that it works as loop
 
 _getOperand:
-        MOV R4, LR
-        SUB SP, SP, #4
-        LDR R0, =prompt_operand
-        BL printf
-        LDR R0, =user_input
-        MOV R1, SP
-        BL scanf
-        LDR R0, [SP]
-        ADD SP, SP, #4
-        MOV PC, R4
+        MOV R4, LR               @ store LR since printf call ourselves
+        SUB SP, SP, #4           @ make room for stack
+        LDR R0, =prompt_operand  @ R0 contains address of format string
+        BL printf                @ call printf
+        LDR R0, =user_input      @ R0 contains address of format sting
+        MOV R1, SP               @ move SP to R1 to store entry of stack
+        BL scanf                 @ call scanf
+        LDR R0, [SP]             @ load value at SP into R0
+        ADD SP, SP, #4           @ restore the stack pointer
+        MOV PC, R4               @ return
 
 GCD_ITERATIVE:
-        MOV R8, R2
-        B _loopThroughMain
+        MOV R8, R2              @ move the (parameter or) R2 to R8
+        B _loopThroughMain      @ branch to _loopThroughMain
         _iterate:
-                MOV R1, R6
-                MOV R2, R7
-                SUB R8, R8, #1
-                B _iterateMain;
+                MOV R1, R6      @ move the value at R6 to R1
+                MOV R2, R7      @ move the value at R7 to R2
+                SUB R8, R8, #1  @ Substract 1 from R8 and copy to R8
+                B _iterateMain; @ branch to _iterateMain
 
         _loopThroghMain:
-                B _switchIterate
+                B _switchIterate        @ branch to _switchIterate
                 
-                _switchIterateOnce:
-                        SUB R1, R1, R8
+                _switchIterateOnce:     
+                        SUB R1, R1, R8  @ Substract R8 from R1 and copy to R1
 
                 _switchIterate:
-                        CMP R1, R8
-                        BHS _switchIterateOnce
+                        CMP R1, R8              @ compare R1 and R8
+                        BHS _switchIterateOnce  @ branch to _switchIterateOnce using Unsigned comparison gave higher or same result
 
-                B _switchIterateTwice
+                B _switchIterateTwice           @ branch to _switchIterateTwice
                 _switchIterate2:
-                        SUB R2, R2, R8
+                        SUB R2, R2, R8          @ Substract R2 from R8 and copy to R2
                         
                 _switchIterateTwice:
-                        CMP R2, R8
-                        BHS _switchIterate2
+                        CMP R2, R8              @ compare R2 and R8
+                        BHS _switchIterate2     @ branch to _switchIterateOnce using Unsigned comparison gave higher or same result
 
-                MOV R5, R1
-                MOV R9, R2
-                CMP R5, #0
-                BNE _iterate
-                MOV R0, R8
-                MOV PC, LR
+                MOV R5, R1                      @ move the value at R1 to R5
+                MOV R9, R2                      @ move the value at R2 to R9
+                CMP R5, #0                      @ compare R1 and 0
+                BNE _iterate                    @ branch not equal to _iterate
+                MOV R0, R8                      @ move the value at R8 to R0
+                MOV PC, LR                      @ return
 
 _return:
-        MOV R0, R8
-        MOV PC, LR
+        MOV R0, R8                              @ move the value at R8 to R0
+        MOV PC, LR                              @ return
 
 _displayPromptComputation:
-        MOV R4, LR
-        LDR R0, =display_format
-        BL printf
-        MOV PC, R4
+        MOV R4, LR                              @ store LR since printf call ourselves
+        LDR R0, =display_format                 @ R0 contains address of format sting
+        BL printf                               @ branch link to printf
+        MOV PC, R4                              @ return
 
 .data
 prompt_operand:
